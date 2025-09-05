d4d857ab26027d4a6e5e96b65af1fc8d8f16bd23
Chimipupu <chiminyanyan@gmail.com>
2025-09-06 04:36:25 +0900

    YMZ294の全レジスタを定義

--- src/YMZ294_CH32V003F4P6_DEV/User/drv_ymz294.c
+++ src/YMZ294_CH32V003F4P6_DEV/User/drv_ymz294.c
@@ -11,7 +11,76 @@
 
 #include "drv_ymz294.h"
 
-void drv_ymz294_init()
+SOUND_FREQ_CH_A REG_SOUND_FREQ_CH_A;
+SOUND_FREQ_CH_A_2 REG_SOUND_FREQ_CH_A_2;
+SOUND_FREQ_CH_B REG_SOUND_FREQ_CH_B;
+SOUND_FREQ_CH_B_2 REG_SOUND_FREQ_CH_B_2;
+SOUND_FREQ_CH_C REG_SOUND_FREQ_CH_C;
+SOUND_FREQ_CH_C_2 REG_SOUND_FREQ_CH_C_2;
+NOISE_FREQ REG_NOISE_FREQ;
+MIXSER REG_MIXSER;
+VOLUME_CTRL_CH_A REG_VOLUME_CTRL_CH_A;
+VOLUME_CTRL_CH_B REG_VOLUME_CTRL_CH_B;
+VOLUME_CTRL_CH_C REG_VOLUME_CTRL_CH_C;
+ENVELOPE_FREQ REG_ENVELOPE_FREQ;
+ENVELOPE_FREQ_2 REG_ENVELOPE_FREQ_2;
+ENVELOPE_TYPE REG_ENVELOPE_TYPE;
+
+uint8_t *p_reg[YMZ294_REG_CNT] = {
+    &REG_SOUND_FREQ_CH_A.BYTE,
+    &REG_SOUND_FREQ_CH_A_2.BYTE,
+    &REG_SOUND_FREQ_CH_B.BYTE,
+    &REG_SOUND_FREQ_CH_B_2.BYTE,
+    &REG_SOUND_FREQ_CH_C.BYTE,
+    &REG_SOUND_FREQ_CH_C_2.BYTE,
+    &REG_NOISE_FREQ.BYTE,
+    &REG_MIXSER.BYTE,
+    &REG_VOLUME_CTRL_CH_A.BYTE,
+    &REG_VOLUME_CTRL_CH_B.BYTE,
+    &REG_VOLUME_CTRL_CH_C.BYTE,
+    &REG_ENVELOPE_FREQ.BYTE,
+    &REG_ENVELOPE_FREQ_2.BYTE,
+    &REG_ENVELOPE_TYPE.BYTE,
+};
+
+static void reg_init_all(void);
+
+static void reg_init_all(void)
 {
-    // TODO
+    uint8_t i;
+
+    for(i = 0; i < YMZ294_REG_CNT; i++)
+    {
+        *p_reg[i] = 0;
+    }
+}
+
+/**
+ * @brief YMZ294のレジスタ値設定関数
+ * 
+ * @param addr YMZ294のレジスタアドレス(0x00~0x0D)
+ * @param val 設定するレジスタ値
+ */
+void drv_ymz294_set_reg(uint8_t addr, uint8_t val)
+{
+    *p_reg[addr] = val;
+    // TODO:YMZ294への書き込み
+}
+
+/**
+ * @brief YMZ294のレジスタ値取得関数
+ * 
+ * @param addr YMZ294のレジスタアドレス(0x00~0x0D)
+ * @return uint8_t 取得したレジスタ値
+ */
+uint8_t drv_ymz294_get_reg(uint8_t addr)
+{
+    uint8_t reg;
+    reg = *p_reg[addr];
+    return reg;
+}
+
+void drv_ymz294_init(void)
+{
+    reg_init_all();
 }
--- src/YMZ294_CH32V003F4P6_DEV/User/drv_ymz294.h
+++ src/YMZ294_CH32V003F4P6_DEV/User/drv_ymz294.h
@@ -16,6 +16,213 @@
 #include <stdbool.h>
 #include <string.h>
 
-void drv_ymz294_init();
+// YMZ294の制御ピン
+#define YMZ294_WR_PIN       
+#define YMZ294_CS_PIN       
+#define YMZ294_A0_PIN       
+#define YMZ294_D0_PIN       
+#define YMZ294_D1_PIN       
+#define YMZ294_D2_PIN       
+#define YMZ294_D3_PIN       
+#define YMZ294_D4_PIN       
+#define YMZ294_D5_PIN       
+#define YMZ294_D6_PIN       
+#define YMZ294_D7_PIN       
+
+// YMZ294のレジスタ
+#define YMZ294_REG_CNT                           0x0E // YMZ294のレジスタ数
+#define YMZ294_REG_CH_A_SOUND_FREQ_ADDR          0x00 // Ch A 楽音周波数レジスタ TP[7:0]
+#define YMZ294_REG_CH_A_SOUND_FREQ_2_ADDR        0x01 // Ch A 楽音周波数レジスタ TP[11:8]
+#define YMZ294_REG_CH_B_SOUND_FREQ_ADDR          0x02 // Ch B 楽音周波数レジスタ TP[7:0]
+#define YMZ294_REG_CH_B_SOUND_FREQ_2_ADDR        0x03 // Ch B 楽音周波数レジスタ TP[11:8]
+#define YMZ294_REG_CH_C_SOUND_FREQ_ADDR          0x04 // Ch C 楽音周波数レジスタ TP[7:0]
+#define YMZ294_REG_CH_C_SOUND_FREQ_2_ADDR        0x05 // Ch C 楽音周波数レジスタ TP[11:8]
+#define YMZ294_REG_NOISE_ADDR                    0x06 // ノイズ周波数レジスタ NP[4:0]
+#define YMZ294_REG_MIXER_ADDR                    0x07 // ミキサーレジスタ
+#define YMZ294_REG_VOLUME_CTRL_CH_A_ADDR         0x08 // 音量コントロールDAC Ch Aレジスタ
+#define YMZ294_REG_VOLUME_CTRL_CH_B_ADDR         0x09 // 音量コントロールDAC Ch Bレジスタ
+#define YMZ294_REG_VOLUME_CTRL_CH_C_ADDR         0x0A // 音量コントロールDAC Ch Cレジスタ
+#define YMZ294_REG_ENVELOPE_FREQ_ADDR            0x0B // エンベロープ周波数レジスタ
+#define YMZ294_REG_ENVELOPE_FREQ_2_ADDR          0x0C // エンベロープ周波数レジスタ
+#define YMZ294_REG_ENVELOPE_TYPE_ADDR            0x0D // エンベロープ形状レジスタ
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t TP0:1;     // bit0
+        uint8_t TP1:1;     // bit1
+        uint8_t TP2:1;     // bit2
+        uint8_t TP3:1;     // bit3
+        uint8_t TP4:1;     // bit4
+        uint8_t TP5:1;     // bit5
+        uint8_t TP6:1;     // bit6
+        uint8_t TP7:1;     // bit7
+    }BIT;
+} SOUND_FREQ_CH_A;
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t TP8:1;          // bit0
+        uint8_t TP9:1;          // bit1
+        uint8_t TP10:1;         // bit2
+        uint8_t TP11:1;         // bit3
+        uint8_t RESERVED:4;     // bit[7:4]
+    }BIT;
+} SOUND_FREQ_CH_A_2;
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t TP0:1;     // bit0
+        uint8_t TP1:1;     // bit1
+        uint8_t TP2:1;     // bit2
+        uint8_t TP3:1;     // bit3
+        uint8_t TP4:1;     // bit4
+        uint8_t TP5:1;     // bit5
+        uint8_t TP6:1;     // bit6
+        uint8_t TP7:1;     // bit7
+    }BIT;
+} SOUND_FREQ_CH_B;
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t TP8:1;          // bit0
+        uint8_t TP9:1;          // bit1
+        uint8_t TP10:1;         // bit2
+        uint8_t TP11:1;         // bit3
+        uint8_t RESERVED:4;     // bit[7:4]
+    }BIT;
+} SOUND_FREQ_CH_B_2;
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t TP0:1;     // bit0
+        uint8_t TP1:1;     // bit1
+        uint8_t TP2:1;     // bit2
+        uint8_t TP3:1;     // bit3
+        uint8_t TP4:1;     // bit4
+        uint8_t TP5:1;     // bit5
+        uint8_t TP6:1;     // bit6
+        uint8_t TP7:1;     // bit7
+    }BIT;
+} SOUND_FREQ_CH_C;
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t TP8:1;          // bit0
+        uint8_t TP9:1;          // bit1
+        uint8_t TP10:1;         // bit2
+        uint8_t TP11:1;         // bit3
+        uint8_t RESERVED:4;     // bit[7:4]
+    }BIT;
+} SOUND_FREQ_CH_C_2;
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t NP0:1;          // bit0
+        uint8_t NP1:1;          // bit1
+        uint8_t NP2:1;          // bit2
+        uint8_t NP3:1;          // bit3
+        uint8_t NP4:1;          // bit4
+        uint8_t RESERVED:3;     // bit[7:5]
+    }BIT;
+} NOISE_FREQ;
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t TONE_A:1;       // bit0
+        uint8_t TONE_B:1;       // bit1
+        uint8_t TONE_C:1;       // bit2
+        uint8_t NOSE_A:1;       // bit3
+        uint8_t NOSE_B:1;       // bit4
+        uint8_t NOSE_C:1;       // bit5
+        uint8_t RESERVED:2;     // bit[7:6]
+    }BIT;
+} MIXSER;
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t L0:1;          // bit0
+        uint8_t L1:1;          // bit1
+        uint8_t L2:1;          // bit2
+        uint8_t L3:1;          // bit3
+        uint8_t M:1;           // bit4
+        uint8_t RESERVED:3;    // bit[7:5]
+    }BIT;
+} VOLUME_CTRL_CH_A;
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t L0:1;          // bit0
+        uint8_t L1:1;          // bit1
+        uint8_t L2:1;          // bit2
+        uint8_t L3:1;          // bit3
+        uint8_t M:1;           // bit4
+        uint8_t RESERVED:3;    // bit[7:5]
+    }BIT;
+} VOLUME_CTRL_CH_B;
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t L0:1;          // bit0
+        uint8_t L1:1;          // bit1
+        uint8_t L2:1;          // bit2
+        uint8_t L3:1;          // bit3
+        uint8_t M:1;           // bit4
+        uint8_t RESERVED:3;    // bit[7:5]
+    }BIT;
+} VOLUME_CTRL_CH_C;
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t EP0:1;     // bit0
+        uint8_t EP1:1;     // bit1
+        uint8_t EP2:1;     // bit2
+        uint8_t EP3:1;     // bit3
+        uint8_t EP4:1;     // bit4
+        uint8_t EP5:1;     // bit5
+        uint8_t EP6:1;     // bit6
+        uint8_t EP7:1;     // bit7
+    }BIT;
+} ENVELOPE_FREQ;
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t EP8:1;      // bit0
+        uint8_t EP9:1;      // bit1
+        uint8_t EP10:1;     // bit2
+        uint8_t EP11:1;     // bit3
+        uint8_t EP12:1;     // bit4
+        uint8_t EP13:1;     // bit5
+        uint8_t EP14:1;     // bit6
+        uint8_t EP15:1;     // bit7
+    }BIT;
+} ENVELOPE_FREQ_2;
+
+typedef union {
+    uint8_t BYTE;
+    struct {
+        uint8_t HOLD:1;        // bit0
+        uint8_t ALT:1;         // bit1
+        uint8_t ATT:1;         // bit2
+        uint8_t CONT:1;        // bit3
+        uint8_t RESERVED:4;    // bit[7:4]
+    }BIT;
+} ENVELOPE_TYPE;
+
+void drv_ymz294_set_reg(uint8_t addr, uint8_t val);
+uint8_t drv_ymz294_get_reg(uint8_t addr);
+void drv_ymz294_init(void);
 
 #endif // DRV_YMZ294_H
